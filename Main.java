5ad523d0ba593c936c2f8f0603b3779db913b43d
prajakta <prajaktashahare1998@gmail.com>
2023-11-05 11:21:31 +0530

    MY FIRST JAVA PROGRAM

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("HELLO CUTIE PRAJAKTA");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+}
